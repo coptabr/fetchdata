ebfd511d65e7a31c64bed3b052542b6ed63b2152
coptabr <89669625+coptabr@users.noreply.github.com>
2022-06-29 17:00:00 -0300

    FirstCommit
    My first commit GIT

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Fetchdata from "./fetchdata";
+
+function App() {
+  return (
+    <div className="App">
+      <Fetchdata />
+
+      {/* 
+      <Fetchdata>
+        <Card />
+      </Fetchdata>
+      */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/fetchdata.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+function FetchData() {
+  let [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/games`)
+      .then((response) => response.json())
+      .then((loadData) => setData(loadData));
+
+    /*     const fetching = async () => {
+      const apiResponse = await fetch("http://localhost:5000/games");
+      const apiResponseJSON = await apiResponse.json();
+      //console.log(apiResponseJSON);
+      setData(apiResponseJSON);
+    };
+    fetching(); */
+  }, []);
+
+  //let retData = JSON.stringify(data);
+
+  return (
+    <>
+      {data.map((item) => (
+        <div key={item.id}>
+          <p>{item.title}</p>
+          <img src={item.thumbnail} />
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default FetchData;
