1cc3958c806798e883c4e619ab1207fcf943d427
coptabr <89669625+coptabr@users.noreply.github.com>
2022-07-05 14:58:54 -0300

    Passing props to card - Start

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,10 +1,21 @@
+import { BsPlusCircleDotted } from "react-icons/bs";
 import classes from "./Card.module.css";
 
 function Card() {
   return (
     <div className={classes.container}>
-      <div className={classes.img}></div>
-      <div className={classes.shortDesc}>ShortDesc</div>
+      <div className={classes.gameImg}></div>
+      <div className={classes.shortDesc}>
+        <div className={classes.details}>
+          <BsPlusCircleDotted className={classes.btnDetails} />
+        </div>
+        <div className={classes.textDesc}>
+          <ul>
+            <li>DIABLO IMORAL</li>
+            <li>BLI$ARD</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
