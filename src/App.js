5f2c4a3c93dea821737b8af962193b618006eba9
coptabr <89669625+coptabr@users.noreply.github.com>
2022-07-09 17:35:31 -0300

    estrutura de layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Fetchdata from "./components/fetch/Fetchdata";
+import Header from "./components/header/Header";
 import Layout from "./layout/Layout";
 
 function App() {
   return (
     <div className="App">
-      <Layout>{/* <Fetchdata /> */}</Layout>
+      <Layout>
+        <Header />
+        {/* <Fetchdata /> */}
+      </Layout>
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import classes from "./Header.module.css";
+
+function Header() {
+  return (
+    <div className={classes.container}>
+      <div>Titulo</div>
+      <div>Componente filtro</div>
+    </div>
+  );
+}
+export default Header;
